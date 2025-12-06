1d104d6781bb01a5c53a353226bde4637db68757
Gregmar4105 <gregmarresurreccion4105@gmail.com>
2025-12-06 17:13:15 +0800

    hey

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -7,6 +7,8 @@ use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
 
 class MessageSent implements ShouldBroadcastNow
 {
