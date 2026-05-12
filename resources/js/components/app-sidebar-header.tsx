42531175fb6f80078985de16b7159b8fdb32487a
Gregmar4105 <gregmarresurreccion4105@gmail.com>
2026-05-12 15:18:46 +0800

    realtime sync to google sheets

--- resources/js/components/app-sidebar-header.tsx
+++ resources/js/components/app-sidebar-header.tsx
@@ -14,6 +14,7 @@ import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
+    DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { usePage, router, Link } from '@inertiajs/react';
 import { useState, useEffect } from 'react';
