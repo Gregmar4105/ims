3b898853fa1bbfcc4e071692811924bcc89af502
Gregmar4105 <gregmarresurreccion4105@gmail.com>
2026-05-12 14:16:40 +0800

    realtime sync to google sheets

--- config/google.php
+++ config/google.php
@@ -2,7 +2,7 @@
 
 return [
     'sheets' => [
-        'service_account_json' => env('GOOGLE_SHEETS_JSON'),
+        'service_account_json' => base_path(env('GOOGLE_SHEETS_JSON')),
         'spreadsheet_id' => env('GOOGLE_SHEETS_LINK_ID'), // We will handle parsing in the service if needed
     ],
 ];
